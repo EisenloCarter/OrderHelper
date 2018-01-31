fca1afe395c80db545d8575818bba2ecc0a9bc07
EisenloCarter <jiafeiheimao@gmail.com>
2018-01-31 16:35:50 +0800

    prepare fixing searching bar

---
+++ OrderHelper/HomePage/Controller/SearchItems.swift
@@ -0,0 +1,14 @@
+//
+//  SearchItems.swift
+//  OrderHelper
+//
+//  Created by spidepa on 31/1/18.
+//  Copyright © 2018年 spidepa. All rights reserved.
+//
+
+import Foundation
+
+struct Candy {
+    let category : String
+    let name : String
+}
---
+++ OrderHelper/HomePage/Controller/SearchTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SearchTableViewController.swift
+//  OrderHelper
+//
+//  Created by spidepa on 31/1/18.
+//  Copyright © 2018年 spidepa. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 6
+    }
+}
